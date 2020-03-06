3683478c2ec2e6a5eb577ec2ef6091d630b4f6ad
SlikRick08 <riki.mahal@kellogg.northwestern.edu>
2020-03-06 06:14:29 +0000

    Added restaurant specific-pages and updated data-base with seed-data

--- createdb.rb
+++ createdb.rb
@@ -6,6 +6,7 @@ DB ||= Sequel.connect(connection_string)                                        
 
 # Database Schema - Reflects Domain Model
 
+#Locations Table
 DB.create_table! :locations do
   primary_key :id
   foreign_key :areas_id
@@ -14,6 +15,7 @@ DB.create_table! :locations do
   String :address
 end
 
+#Riki's Table
 DB.create_table! :rikis do
   primary_key :id
   foreign_key :locations_id
@@ -24,11 +26,13 @@ DB.create_table! :rikis do
   String :comments
 end
 
+#Areas Table
 DB.create_table! :areas do
   primary_key :id
   String :name
 end
 
+#Users Table
 DB.create_table! :users do
   primary_key :id
   String :name
@@ -39,21 +43,92 @@ end
 # Areas Initial (Seed) Data
 areas_table = DB.from(:areas)
 areas_table.insert(name: "Evanston")
-areas_table.insert(name: "Bucktown")
 areas_table.insert(name: "Lincoln Park")
-areas_table.insert(name: "Logan Square")
-areas_table.insert(name: "The Loop")
 areas_table.insert(name: "Old Town")
 areas_table.insert(name: "River North")
-areas_table.insert(name: "Rogers Park")
-areas_table.insert(name: "Streeterville")
 areas_table.insert(name: "Wicker Park")
 areas_table.insert(name: "West Loop")
 
-#Locations Initial (Seed)
+#Locations Initial (Seed) - Evanston
 
 locations_table = DB.from(:locations)
-locations_table.insert(areas_id: 1,
-                    name: "Whiskey Thief", 
+locations_table.insert(name: "Whiskey Thief", 
                     description: "Dark and Sinister",
-                    address: "Kellogg Global Hub")
+                    address: "616 Davis St, Evanston, IL 60201",
+                    areas_id: 1)
+
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Smylie Brothers", 
+                    description: "Great BBQ & Brew",
+                    address: "1615 Oak Ave, Evanston, IL 60201",
+                    areas_id: 1)
+
+#Locations Initial (Seed) - Lincoln Park
+
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Delilah's Chicago", 
+                    description: "300 Varieties of Whiskey",
+                    address: "2771 N Lincoln Ave, Chicago, IL 60614",
+                    areas_id: 2)
+
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Alinea", 
+                    description: "Save It For The One...",
+                    address: "1723 N Halsted St, Chicago, IL 60614",
+                    areas_id: 2)    
+
+#Locations Initial (Seed) - Old Town
+
+locations_table = DB.from(:locations)
+locations_table.insert(name: "The VIG Chicago", 
+                    description: "Warehouse + Gastropub Feel",
+                    address: "1527 N Wells St, Chicago, IL 60610",
+                    areas_id: 3)
+
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Old Town Ale House", 
+                    description: "Cash-Only",
+                    address: "219 W North Ave, Chicago, IL 60610",
+                    areas_id: 3)    
+
+#Locations Initial (Seed) - River North
+
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Untitled", 
+                    description: "Huge Bar! A Kellogg Staple...",
+                    address: "111 W Kinzie St, Chicago, IL 60654",
+                    areas_id: 4)
+
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Bub City", 
+                    description: "Live Country Music!",
+                    address: "435 N Clark St, Chicago, IL 60654",
+                    areas_id: 4)    
+
+#Locations Initial (Seed) - Wicker Park
+
+locations_table = DB.from(:locations)
+locations_table.insert(name: "The Violet Hour", 
+                    description: "Chic Speakeasy",
+                    address: "1520 N Damen Ave, Chicago, IL 60622",
+                    areas_id: 5)
+
+locations_table = DB.from(:locations)
+locations_table.insert(name: "The Revel Room", 
+                    description: "Vintage Furnishings and Walls of Books",
+                    address: "1566 N Milwaukee Ave, Chicago, IL 60622",
+                    areas_id: 5)   
+
+#Locations Initial (Seed) - West Loop
+
+locations_table = DB.from(:locations)
+locations_table.insert(name: "Moneygun", 
+                    description: "Loud Rap Music!",
+                    address: "660 W Lake St",
+                    areas_id: 6)
+
+locations_table = DB.from(:locations)
+locations_table.insert(name: "The Aviary", 
+                    description: "Over-the-Top Cocktails",
+                    address: "955 W Fulton Market, Chicago, IL 60607",
+                    areas_id: 6)   
